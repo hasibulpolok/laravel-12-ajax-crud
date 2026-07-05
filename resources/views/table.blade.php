36d1eb2f5c1cbb66db08fad0efedc40a742583f7
hasibulpolok <hasibulpolok.bdn@gmail.com>
2026-07-05 10:38:20 +0600

    added edit delete btn

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,27 +1,38 @@
- <table class="table table-bordered table-hover">
-            <thead class="table-dark">
-                <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                </tr>
-            </thead>
+<table class="table table-bordered table-hover">
 
-            <tbody>
-                @foreach ($students as $student)
-                    <tr>
+    <thead class="table-dark">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Action</th>
+        </tr>
+    </thead>
 
-                        <td>{{ $student->id }}</td>
-                        <td>{{ $student->name }}</td>
-                        <td>{{ $student->email }}</td>
-                        <td>{{ $student->address }}</td>
+    <tbody>
 
-                    </tr>
-                @endforeach
+        @foreach($students as $student)
 
+        <tr>
+            <td>{{ $student->id }}</td>
+            <td>{{ $student->name }}</td>
+            <td>{{ $student->email }}</td>
+            <td>{{ $student->address }}</td>
 
+            <td>
+                <button type="button" class="btn btn-primary btn-sm">
+                    Edit
+                </button>
 
+                <button type="button" class="btn btn-danger btn-sm">
+                    Delete
+                </button>
+            </td>
+        </tr>
 
-            </tbody>
-        </table>
+        @endforeach
+
+    </tbody>
+
+</table>
